d01100088fc3596db69850d4cfc33fe8b43ec91c
Ronny Hildebrandt <ronny.hildebrandt@avorium.de>
2018-11-26 21:31:33 +0100

    Implemented JSON handling on server

--- server/index.js
+++ server/index.js
@@ -2,10 +2,37 @@ const WebSocket = require('ws');
 
 const wss = new WebSocket.Server({ port: 42001 });
 
+const Arrange = {
+    createField: function (socket, data) { console.log(socket, data); },
+    createObject: function (socket, data) { console.log(socket, data); },
+    createType: function (socket, data) { console.log(socket, data); },
+    createUser: function (socket, data) { console.log(socket, data); },
+    deleteField: function (socket, data) { console.log(socket, data); },
+    deleteObject: function (socket, data) { console.log(socket, data); },
+    deleteType: function (socket, data) { console.log(socket, data); },
+    deleteUser: function (socket, data) { console.log(socket, data); },
+    login: function (socket, data) { console.log(socket, data); },
+    logout: function (socket, data) { console.log(socket, data); },
+    updateField: function (socket, data) { console.log(socket, data); },
+    updateObject: function (socket, data) { console.log(socket, data); },
+    updateType: function (socket, data) { console.log(socket, data); },
+    updateUser: function (socket, data) { console.log(socket, data); },
+}
+
 wss.on('connection', function connection(ws) {
-  console.log('Connected');
-  ws.on('message', function incoming(message) {
-    console.log('received: %s', message);
-  });
-  ws.send('Geh pupsen!');
+
+    ws.on('message', function incoming(message) {
+        try {
+            const data = JSON.parse(message);
+            const keys = Object.keys(data);
+            if (keys.length !== 1) return;
+            const functionName = keys[0];
+            if (!Arrange[functionName]) return
+            Arrange[functionName](this, data[functionName]);
+        } catch (ex) {
+            console.error(ex);
+        }
+    });
+
+    ws.send('Geh strullern!');
 });
