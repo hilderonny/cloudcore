d38b2a3fa10bf6a83ed66366330e2c48c57eb73b
Ronny Hildebrandt <ronny.hildebrandt@avorium.de>
2018-11-30 20:44:42 +0100

    fixed startup

--- server/index.js
+++ server/index.js
@@ -1,7 +1,14 @@
+const fs = require('fs');
+const https = require('https');
 
 const Db = require('./db')('127.0.0.1:27017');
 
-const SocketServer = require('./socketServer')({ port: 42001 });
+const app = https.createServer({
+        cert: fs.readFileSync('/etc/letsencrypt/live/avorium.de/fullchain.pem'),
+        key: fs.readFileSync('/etc/letsencrypt/live/avorium.de/privkey.pem')
+}).listen(42001);
+
+const SocketServer = require('./socketServer')({ server: app });
 
 // Register message handlers
 require('./messagehandlers/mongodbhandler')(Db, SocketServer);
