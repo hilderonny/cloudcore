c8602a2cd78335d69432843143da6f2cc31208e1
Ronny Hildebrandt <ronny.hildebrandt@avorium.de>
2019-01-22 14:47:33 +0100

    Added gitlab runner and blanko test.

---
+++ test/api/login.js
@@ -0,0 +1,16 @@
+
+describe('API login', function() {
+
+    before(async function() {
+
+    });
+
+    beforeEach(async function() {
+
+    });
+
+    xit('Responds with error 400 when no username and no password is given', async function() {
+
+    });
+
+});
