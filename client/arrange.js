e8d95ed317d73ba965ff640107a4c04e6ca4d1cb
Ronny <ronny@Ronnys-MacBook-Air.local>
2019-02-02 13:21:46 +0100

    Added correct login handling.

--- client/arrange.js
+++ client/arrange.js
@@ -2,20 +2,32 @@ let $arr = {
 
     currentuser: {
         _id: undefined,
+        password: undefined,
+        username: undefined,
         token: undefined
     },
 
     helper: {
-        delete: async function(url, token) {
-            return $arr.helper.request('DELETE', url, token);
+        delete: async function(url) {
+            return $arr.helper.request('DELETE', url, $arr.currentuser.token);
         },
-        get: async function(url, token) {
-            return $arr.helper.request('GET', url, token);
+        get: async function(url) {
+            return $arr.helper.request('GET', url, $arr.currentuser.token);
         },
-        post: async function post(url, token, data) {
-            return $arr.helper.request('POST', url, token, data);
+        handleloginregister: function(username, password, response) {
+            if (response.status === 200) {
+                $arr.currentuser.password = password;
+                $arr.currentuser.username = username;
+                $arr.currentuser._id = response.body._id;
+                $arr.currentuser.token = response.body.token;
+                return true;
+            }
+            return false;
         },
-        request: async function(mode, url, token, data) {
+        post: async function post(url, data) {
+            return $arr.helper.request('POST', url, $arr.currentuser.token, data);
+        },
+        request: async function(mode, url, data) {
             return new Promise(function(resolve) {
                 var xmlhttp = new XMLHttpRequest();
                 xmlhttp.onreadystatechange = function() {
@@ -39,49 +51,51 @@ let $arr = {
     },
 
     addreadableby: async function(tablename, entityid, userid) {
-        return $arr.helper.post('/api/arrange/addreadableby/' + tablename + '/' + entityid, currentuser.token, { userid: userid });
+        return $arr.helper.post('/api/arrange/addreadableby/' + tablename + '/' + entityid, { userid: userid });
     },
     addwritableby: async function(tablename, entityid, userid) {
-        return $arr.helper.post('/api/arrange/addwritableby/' + tablename + '/' + entityid, currentuser.token, { userid: userid });
+        return $arr.helper.post('/api/arrange/addwritableby/' + tablename + '/' + entityid, { userid: userid });
     },
     delete: async function(tablename, entityid) {
-        return $arr.helper.delete('/api/arrange/delete/' + tablename + '/' + entityid, currentuser.token);
+        return $arr.helper.delete('/api/arrange/delete/' + tablename + '/' + entityid);
     },
     details: async function(tablename, entityid, attributefilter) {
-        return $arr.helper.post('/api/arrange/details/' + tablename + '/' + entityid, currentuser.token, attributefilter);
+        return $arr.helper.post('/api/arrange/details/' + tablename + '/' + entityid, attributefilter);
     },
     list: async function(tablename, queryfilter) {
-        return $arr.helper.post('/api/arrange/list/' + tablename, currentuser.token, queryfilter);
+        return $arr.helper.post('/api/arrange/list/' + tablename, queryfilter);
     },
     listusers: async function() {
-        return $arr.helper.get('/api/arrange/listusers', currentuser.token);
+        return $arr.helper.get('/api/arrange/listusers');
     },
     login: async function(username, password) {
-        return $arr.helper.post('/api/arrange/login', undefined, { username: username, password: password });
+        const response = await $arr.helper.post('/api/arrange/login', { username: username, password: password });
+        return $arr.helper.handleloginregister(username, password, response);
     },
     register: async function(username, password) {
-        return $arr.helper.post('/api/arrange/register', undefined, { username: username, password: password });
+        const response = $arr.helper.post('/api/arrange/register', { username: username, password: password });
+        return $arr.helper.handleloginregister(username, password, response);
     },
     removereadableby: async function(tablename, entityid, userid) {
-        return $arr.helper.post('/api/arrange/removereadableby/' + tablename + '/' + entityid, currentuser.token, { userid: userid });
+        return $arr.helper.post('/api/arrange/removereadableby/' + tablename + '/' + entityid, { userid: userid });
     },
     removewritableby: async function(tablename, entityid, userid) {
-        return $arr.helper.post('/api/arrange/removewritableby/' + tablename + '/' + entityid, currentuser.token, { userid: userid });
+        return $arr.helper.post('/api/arrange/removewritableby/' + tablename + '/' + entityid, { userid: userid });
     },
     save: async function(tablename, entity) {
-        return $arr.helper.post('/api/arrange/save/' + tablename, currentuser.token, entity);
+        return $arr.helper.post('/api/arrange/save/' + tablename, entity);
     },
     setpassword: async function(newpassword) {
-        return $arr.helper.post('/api/arrange/setpassword', currentuser.token, { password: newpassword });
+        return $arr.helper.post('/api/arrange/setpassword', { password: newpassword });
     },
     setpubliclyreadable: async function(tablename, entityid, readable) {
-        return $arr.helper.post('/api/arrange/setpubliclyreadable/' + tablename + '/' + entityid + '/' + (readable ? 'true' : 'false'), currentuser.token);
+        return $arr.helper.post('/api/arrange/setpubliclyreadable/' + tablename + '/' + entityid + '/' + (readable ? 'true' : 'false'));
     },
     setpubliclywritable: async function(tablename, entityid, writable) {
-        return $arr.helper.post('/api/arrange/setpubliclywritable/' + tablename + '/' + entityid + '/' + (writable ? 'true' : 'false'), currentuser.token);
+        return $arr.helper.post('/api/arrange/setpubliclywritable/' + tablename + '/' + entityid + '/' + (writable ? 'true' : 'false'));
     },
     transferownership: async function(tablename, entityid, newuserid) {
-        return $arr.helper.post('/api/arrange/transferownership/' + tablename + '/' + entityid + '/' + newuserid, currentuser.token);
+        return $arr.helper.post('/api/arrange/transferownership/' + tablename + '/' + entityid + '/' + newuserid);
     }
 
 };
