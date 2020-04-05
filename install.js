e41f38ee5b09a93bb206841c6a9d28b5cbbc2217
root <root@localhost.localdomain>
2020-04-05 19:36:56 +0000

    Entity deletion implemented

--- install.js
+++ install.js
@@ -11,9 +11,11 @@ var fs = require('fs');
         await client.query("ALTER TABLE views ADD COLUMN IF NOT EXISTS content VARCHAR;");
         await client.query("ALTER TABLE views ADD COLUMN IF NOT EXISTS contenttype VARCHAR(255);");
         await client.query("ALTER TABLE views ADD COLUMN IF NOT EXISTS url VARCHAR(255) UNIQUE;");
+        await client.query("ALTER TABLE views ADD COLUMN IF NOT EXISTS package VARCHAR(255);");
         await client.query("CREATE TABLE IF NOT EXISTS routers (id SERIAL);");
         await client.query("ALTER TABLE routers ADD COLUMN IF NOT EXISTS code VARCHAR;");
         await client.query("ALTER TABLE routers ADD COLUMN IF NOT EXISTS url VARCHAR(255) UNIQUE;");
+        await client.query("ALTER TABLE routers ADD COLUMN IF NOT EXISTS package VARCHAR(255);");
 
         // Standardviews vorbereiten
         async function insertView(url, contenttype, content) {
@@ -22,7 +24,7 @@ var fs = require('fs');
         await insertView('/', 'text/html', '<a href="/vger">VGER</a>');
 
         // Demo-Router in Datenbank eintragen. Reagiert auf /fubbele/eins und /fubbele/zwei/:zahl und /fubbele/hubbele/:tablename
-        await client.query("INSERT INTO routers (url, code) VALUES ($1, $2) ON CONFLICT (url) DO NOTHING;", ['/fubbele', fs.readFileSync(__dirname + '/templates/router.js')]);
+        await client.query("INSERT INTO routers (url, code) VALUES ($1, $2) ON CONFLICT (url) DO NOTHING;", ['/fubbele', fs.readFileSync(__dirname + '/vger/router.js')]);
 
         await client.end();
     } catch (ex) {
