56f1c665ab30dc8f673bc87e05256787e97fc8e7
root <root@localhost.localdomain>
2020-04-16 19:18:45 +0000

    install Skript angepasst

--- install.js
+++ install.js
@@ -1,5 +1,4 @@
 var pg = require('pg');
-var fs = require('fs');
 
 (async () => {
     try {
@@ -8,19 +7,6 @@ var fs = require('fs');
 
         // UUID-Erweiterung anlegen
         await client.query('CREATE EXTENSION IF NOT EXISTS "uuid-ossp";');
-
-        // Tabellen anlegen
-        await client.query("CREATE TABLE IF NOT EXISTS views (id UUID PRIMARY KEY DEFAULT uuid_generate_v4());");
-        await client.query("ALTER TABLE views ADD COLUMN IF NOT EXISTS content VARCHAR;");
-        await client.query("ALTER TABLE views ADD COLUMN IF NOT EXISTS contenttype VARCHAR(255);");
-        await client.query("ALTER TABLE views ADD COLUMN IF NOT EXISTS url VARCHAR(255) UNIQUE;");
-        await client.query("CREATE TABLE IF NOT EXISTS routers (id UUID PRIMARY KEY DEFAULT uuid_generate_v4());");
-        await client.query("ALTER TABLE routers ADD COLUMN IF NOT EXISTS code VARCHAR;");
-        await client.query("ALTER TABLE routers ADD COLUMN IF NOT EXISTS url VARCHAR(255) UNIQUE;");
-
-        // TODO: Paketupload als einzige API nativ bereitstellen
-        // Standardviews vorbereiten
-        await client.query("INSERT INTO views (url, contenttype, content) VALUES ($1, $2, $3) ON CONFLICT (url) DO NOTHING;", ['/', 'text/html', '<a href="/vger">VGER</a>']);
 
         await client.end();
     } catch (ex) {
