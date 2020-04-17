6a75784314e0f161462d697220807bb8c4142f40
root <root@localhost.localdomain>
2020-04-17 19:48:00 +0000

    Backup

--- backup.sh
+++ backup.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
-# Erstellt DB-Dumps mit aktuallem Datum.
+# Erstellt DB-Dumps mit aktuallem Datum und commited dieses automatisch.
 
+cd /root/gitlab/hilderonny/cloudcore
+git pull
 wget http://localhost/backup/ -O ./backup/$(date +"%Y-%m-%d").sql
+git add .
+git commit -m "Backup"
+git push
