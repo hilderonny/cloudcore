8a6b836d067c7eb17e3b821cc6a99c9d42eb6442
root <root@localhost.localdomain>
2020-04-19 19:52:43 +0000

    users-Tabelle in Paket auth verschoben

--- install.js
+++ install.js
@@ -8,15 +8,6 @@ var pg = require('pg');
         // UUID-Erweiterung anlegen
         await client.query('CREATE EXTENSION IF NOT EXISTS "uuid-ossp";');
 
-        // Tabellen anlegen
-        await client.query("CREATE TABLE IF NOT EXISTS users (id UUID PRIMARY KEY DEFAULT uuid_generate_v4());");
-        await client.query("ALTER TABLE users ADD COLUMN IF NOT EXISTS username TEXT;");
-        await client.query("ALTER TABLE users DROP CONSTRAINT IF EXISTS username;");
-        await client.query("ALTER TABLE users ADD CONSTRAINT username UNIQUE (username);");
-
-        // Standardbenutzer 'system' vorbereiten, der dient als Besitzer für alle allgemeinen Datensätze
-        await client.query("INSERT INTO users (username) VALUES ('system') ON CONFLICT (username) DO NOTHING;");
-
         await client.end();
     } catch (ex) {
         console.log(ex);
