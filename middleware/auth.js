e3eae552ad0615048db5320d6e0601a767390bb2
root <root@localhost.localdomain>
2020-04-19 14:54:36 +0000

    SQL parser wieder rausgeworfen

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,13 +1,4 @@
-var queryparser = require('pg-query-parser');
-
 module.exports = async (req, _, next) => {
-    var directquery = req.db.query;
-    req.db.query = async (query) => {
-        var parsedquery = queryparser.parse(query).query;
-        var deparsedquery = queryparser.deparse(parsedquery);
-        console.log(query, parsedquery, deparsedquery);
-        return await directquery(query);
-    }
     if (!req.user) {
         // Erst mal nur Standarduser 'system' benutzen
         // TODO: Token validieren und korrekten User ermitteln
