34b27f7edd032862bd4ab95af937961e213b830b
Ronny <ronny@MacBook-Pro.fritz.box>
2020-05-07 21:54:29 +0200

    hmpf 1

--- tests/testdb.js
+++ tests/testdb.js
@@ -17,20 +17,30 @@ console.log(config);
 
 module.exports = {
     prepare: async () => {
-        if (fs.existsSync('./config.json')) {
-            fs.renameSync('./config.json', './config.json.orig');
-        }
-        fs.writeFileSync('./config.json', JSON.stringify(config));
-        db = new pg.Client({
-            user: config.PGUSER,
-            host: config.PGHOST,
-            database: config.PGDATABASE,
-            password: config.PGPASSWORD,
-            port: config.PGPORT,
-        });
-        await db.connect();
-        for (var tablename of (await db.query("select tablename from pg_tables where schemaname='public';")).rows.map(r => r.tablename)) {
-            await db.query("drop table " + tablename + " cascade;");
+        try {
+            console.log('A1');
+            if (fs.existsSync('./config.json')) {
+                fs.renameSync('./config.json', './config.json.orig');
+            }
+            console.log('A2');
+            fs.writeFileSync('./config.json', JSON.stringify(config));
+            console.log('A3');
+            db = new pg.Client({
+                user: config.PGUSER,
+                host: config.PGHOST,
+                database: config.PGDATABASE,
+                password: config.PGPASSWORD,
+                port: config.PGPORT,
+            });
+            console.log('A4');
+            await db.connect();
+            console.log('A5');
+            for (var tablename of (await db.query("select tablename from pg_tables where schemaname='public';")).rows.map(r => r.tablename)) {
+                await db.query("drop table " + tablename + " cascade;");
+            }
+            console.log('A6');
+        } catch (ex) {
+            console.log(ex);
         }
     },
     query: async (sql) => {
